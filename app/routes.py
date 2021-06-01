f0b7a118d47985dca535bfeb93ea271fdc71845a
PhucDuong-SSS <phucdndev@gmail.com>
2021-06-01 08:55:17 +0700

    tempalte in python use extends

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return '''Hello world!'''
+    user = {"username": "an"}
+    posts = [
+        {
+            'author': {'username':'le'},
+            'body' : 'Beautiful day'
+        },
+        {
+            'author': {'username': 'le'},
+            'body': 'The Avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title="Home", user = user, posts =posts)
+
